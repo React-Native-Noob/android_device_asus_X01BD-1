0987721960eee720ea80f7b33df662a4c9ce027d
Rahul Krishna <rahulkrishna585@gmail.com>
2020-07-12 12:17:17 +0200

    Bringup Corvus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_X01BD.mk
+    $(LOCAL_DIR)/du_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_X01BD-eng \
-    xtended_X01BD-user \
-    xtended_X01BD-userdebug
+    du_X01BD-eng \
+    du_X01BD-user \
+    du_X01BD-userdebug
